6019c78dcc980ca83afcd755d07e162f2168d432
klay <793336993@qq.com>
2022-09-02 22:30:23 +0800

    feat: 修改Footer

--- components/Footer.js
+++ components/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{`© ${2021 - new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
