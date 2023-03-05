1420da725f07999fb641a93052a37e2e9e6b4770
klay <793336993@qq.com>
2023-03-05 15:38:39 +0800

    chore: 简化 Git 提交类型选项列表，并添加交互式命令行提示

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -20,28 +20,6 @@ module.exports = {
       generatedSelectByAI: '选择一个 AI 生成的简短描述:',
       confirmCommit: '是否提交或修改commit ?',
     },
-    types: [
-      { value: 'feat', name: 'feat（特性）:     新增功能' },
-      { value: 'bugfix', name: 'bugfix（修复）:     修复缺陷' },
-      { value: 'docs', name: 'docs（文档）:     文档变更' },
-      {
-        value: 'style',
-        name: 'style（格式）:     代码格式（不影响功能，例如空格、分号等格式修正）',
-      },
-      { value: 'refactor', name: 'refactor（重构）:     代码重构（不包括 bug 修复、功能新增）' },
-      { value: 'perf', name: 'perf（性能）:     性能优化' },
-      { value: 'test', name: 'test（测试）:     添加疏漏测试或已有测试改动' },
-      {
-        value: 'build',
-        name: 'build（构建）:     构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）',
-      },
-      { value: 'ci', name: 'ci（集成）:     修改 CI 配置、脚本' },
-      { value: 'revert', name: 'revert（回退）:     回滚 commit' },
-      {
-        value: 'chore',
-        name: 'chore（其他）:     对构建过程或辅助工具和库的更改（不影响源文件、测试用例）',
-      },
-    ],
     useEmoji: false,
     emojiAlign: 'center',
     themeColorCode: '',
