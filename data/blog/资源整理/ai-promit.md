553004263ac95859e3967e5ba72e30e494a55f7a
klay <793336993@qq.com>
2023-09-03 17:04:11 +0800

    feat: weekly upadte

---
+++ data/blog/资源整理/ai-promit.md
@@ -0,0 +1,11 @@
+---
+title: ai-prompt
+date: '2023-09-03'
+tags: ['资源整理']
+draft: false
+summary: ''
+---
+
+## 如何绕过 LLM 对 API 的道德审查，实现比较稳定的 NSFW（Not Safe For Work）的模拟角色对话？
+
+https://twitter.com/dotey/status/1696952068827754705
--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -5,6 +5,7 @@ const headerNavLinks = [
   { href: '/blog/资源整理/tech-manual', title: '手册' },
   { href: '/blog/资源整理/tech-note', title: '笔记' },
   { href: '/blog/资源整理/tools-collection', title: '工具' },
+  { href: '/blog/资源整理/ai-prompt', title: 'AI' },
   { href: '/about', title: '关于' },
 ]
 
